d38e866b6e734e7c17be5e1f12d5eab0e4387f42
cemujax <cemujax@gmail.com>
2018-11-15 17:28:21 +0900

    Updates

---
+++ precache-manifest.9db506a67c61843fae7c5349b9e5904e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "193bb37d6fe218a71df8",
+    "url": "/movie-app/static/js/main.193bb37d.chunk.js"
+  },
+  {
+    "revision": "1c52b03d2b10547f9bd6",
+    "url": "/movie-app/static/js/1.1c52b03d.chunk.js"
+  },
+  {
+    "revision": "193bb37d6fe218a71df8",
+    "url": "/movie-app/static/css/main.17005cb2.chunk.css"
+  },
+  {
+    "revision": "923bd544d3a74f62ea3ff789645c1fdd",
+    "url": "/movie-app/index.html"
+  }
+];
